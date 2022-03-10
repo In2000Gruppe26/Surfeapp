60044cd69a1cb8207d9abac06f9b6b1335a32810
Andrea <andreaausteng.github.io>
2022-03-10 14:04:57 +0100

    Test på pushe

--- app/src/main/java/com/example/surfeapp/MainActivity.kt
+++ app/src/main/java/com/example/surfeapp/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //test
-        // Dette er en test
+        // Test på å pushe
     }
 }
