304f8657bb776db274a84cbc7449b91c07b06cb8
Magnus Gretton <magngre.github.io>
2022-03-27 22:01:37 +0200

    Lagt til resource file for GM activity_maps.xml - inneholder nøkkelen lagt inn inn MapsActivity.kt som egen fil, har problemer med binding på egen maskin. lagt til eksempel for hustadvika MapsActivity blir ikke gjenkjent (?)

---
+++ app/src/main/java/com/example/surfeapp/MapsActivity.kt
@@ -0,0 +1,41 @@
+package com.example.testmaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.surfeapp.R
+import android.databinding.DataBindingUtil
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.testmaps.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val Hustadvika = LatLng(62.0, 6.0)
+        mMap.addMarker(MarkerOptions().position(Hustadvika).title("Marker in Hustadvika"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Hustadvika))
+    }
+}
