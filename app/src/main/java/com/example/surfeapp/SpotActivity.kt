7af7b1454a3e4e7c5f0f6ce11c482335e183da05
venuslivary <venuslivary.github.io>
2022-04-05 10:28:08 +0200

    Midlertidig commit.

--- app/src/main/java/com/example/surfeapp/SpotActivity.kt
+++ app/src/main/java/com/example/surfeapp/SpotActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.activity.viewModels
 import org.w3c.dom.Text
@@ -23,19 +24,28 @@ class SpotActivity : AppCompatActivity() {
          val viewModel: SpotActivityViewModel by viewModels()
 
          val tekstNavn = findViewById<TextView>(R.id.navnTekst)
-         val tekstBolge = findViewById<TextView>(R.id.bolgeTekst)
-         val tekstVind = findViewById<TextView>(R.id.vindTekst)
+         val tekstBolge1 = findViewById<TextView>(R.id.bolgeTekst1)
+         val tekstBolge2 = findViewById<TextView>(R.id.bolgeTekst2)
+         val tekstBolge3 = findViewById<TextView>(R.id.bolgeTekst3)
+         val tekstVind1 = findViewById<TextView>(R.id.vindTekst1)
+         val tekstVind2 = findViewById<TextView>(R.id.vindTekst2)
          val tekstTemp = findViewById<TextView>(R.id.tempTekst)
          val tekstBes = findViewById<TextView>(R.id.besTekst)
+         val rating = findViewById<RatingBar>(R.id.rating1)
 
 
 
          viewModel.getSurfespots().observe(this) {
              val strand = it.list[1]
+             val ratingRes = strand.getRating()
+             rating.rating = ratingRes.toFloat()
              tekstNavn.text = strand.name
              val cond = strand.getConditions()
-             tekstBolge.text = cond.waveSize.toString()
-             tekstVind.text = cond.wind_speed.toString()
+             tekstBolge1.text = cond.waveSize.toString()
+             tekstBolge2.text = cond.currentSpeed.toString()
+             tekstBolge3.text = cond.currentDirection.toString()
+             tekstVind1.text = cond.wind_speed.toString()
+             tekstVind2.text = cond.wind_from_direction.toString()
              tekstTemp.text = cond.air_temperature.toString()
              tekstBes.text = it.list[0].description.toString()
          }
