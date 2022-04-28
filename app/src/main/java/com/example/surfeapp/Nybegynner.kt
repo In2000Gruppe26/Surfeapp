ab1adfb4b8101bfec219f910689a0bc6da8110de
magnusgretton <magngre@uio.no>
2022-04-28 12:31:37 +0200

    tilbakeknapp - ikke testet - avd funker ikke av en eller annen grunn

--- app/src/main/java/com/example/surfeapp/Nybegynner.kt
+++ app/src/main/java/com/example/surfeapp/Nybegynner.kt
@@ -7,5 +7,17 @@ class Nybegynner : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nybegynner)
+        //actionbar
+        val actionbar = supportActionBar
+        //set actionbar title
+        actionbar!!.title = "Nybegynner?"
+        //set back button
+        actionbar.setDisplayHomeAsUpEnabled(true)
+        actionbar.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
--- app/src/main/java/com/example/surfeapp/TipsOgTriks.kt
+++ app/src/main/java/com/example/surfeapp/TipsOgTriks.kt
@@ -7,5 +7,18 @@ class TipsOgTriks : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tips_og_triks)
+
+        //actionbar
+        val actionbar = supportActionBar
+        //set actionbar title
+        actionbar!!.title = "Tips og triks"
+        //set back button
+        actionbar.setDisplayHomeAsUpEnabled(true)
+        actionbar.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
