b91dac8a3db3cc8d35a09dd07914af9198da9687
Alfred Løvgren <alfredlovgr1@gmail.com>
2022-05-13 10:52:01 +0200

    Asynkront call

--- app/src/test/java/com/example/surfeapp/DataSourceTest.kt
+++ app/src/test/java/com/example/surfeapp/DataSourceTest.kt
@@ -1,22 +1,13 @@
 package com.example.surfeapp
 
-import android.app.Activity
 import android.content.Context
-import android.provider.Settings.Global.getString
-import androidx.test.InstrumentationRegistry.getTargetContext
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.platform.app.InstrumentationRegistry
-import com.google.android.material.internal.ContextUtils.getActivity
 import junit.framework.TestCase
-import org.junit.Before
-import java.lang.Boolean
-import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import android.os.Build.VERSION_CODES.Q
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.common.truth.Truth.assertThat
 import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
@@ -46,6 +37,6 @@ class DataSourceTest {
         val spot:Surfespot = DataSource().getSpots(context)?.list?.get(0)!!
         //Denne testen er litt håpløs fordi den sammenligner en statisk verdi med en variabel. Så her må expected justeres ihh til ekte data.
         print(spot.getConditions())
-        TestCase.assertEquals(1, spot.getRating())
+        TestCase.assertEquals(1, spot.getRating(cond.waveSize, cond.currentSpeed))
     }
 }
