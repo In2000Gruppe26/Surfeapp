423bbdb327999461637a22c552cb1217a45c3a6b
magnusgretton <magngre@uio.no>
2022-05-16 13:44:39 +0200

    ordnet tilgang til de nye sidene. ikke testet

--- app/src/main/java/com/example/surfeapp/Topp5Activity.kt
+++ app/src/main/java/com/example/surfeapp/Topp5Activity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class Top5Activity : AppCompatActivity() {
+class Topp5Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top5)
